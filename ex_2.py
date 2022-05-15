9eef1e74948831d42bd0acc96585ae85f03f2702
estherwahnon <estherwa@hotmail.es>
2022-05-15 16:41:09 +0300

    ex2-3-4 ready

---
+++ ex_2.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def ex_numpy_second():
+    """
+    Function  that creates a vector of length 10 with values evenly distributed between 5 and 50.
+    """
+    v = np.linspace(10, 49, 5)
+    print("Length 10 with values evenly distributed between 5 and 50:")
+    print(v)
--- main.py
+++ main.py
@@ -1,5 +1,17 @@
 from ex_1 import ex_numpy
+from ex_2 import ex_numpy_second
+from ex_3 import ex_numpy_third
+from ex_4 import ex_numpy_forth
 
 if __name__ == '__main__':
     # ex1
     ex_numpy()
+    # ex2
+    ex_numpy_second()
+    # ex3
+    ex_numpy_third()
+    # ex4
+    ex_numpy_forth()
+
+
+
