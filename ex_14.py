4a1a9531967f590ec05ad52aedeb762b36217df7
Esther Wahnon Amselem <82866031+estherwa@users.noreply.github.com>
2022-06-14 20:56:02 +0300

    Update ex_14.py

--- ex_14.py
+++ ex_14.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def ex_numpy_fourteen(m1, m2):
     """
-     A NumPy program to combine a one and a two-dimensional array together and display their elements.
+    A NumPy program to combine a one and a two-dimensional array together and display their elements.
     """
     for first_matrix_number, second_matrix_number in np.nditer([m1, m2]):
         print(first_matrix_number, ":", second_matrix_number)
