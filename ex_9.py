3f95870dad4545be3ed2ecd3c546bc78fe556a91
Esther Wahnon Amselem <82866031+estherwa@users.noreply.github.com>
2022-06-14 20:50:51 +0300

    Update ex_9.py

--- ex_9.py
+++ ex_9.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def ex_numpy_nine(num1, num2):
     """
-       Function to program to Write a NumPy program to multiply two given arrays of same size element-by-element..
-       """
+    Function to program to Write a NumPy program to multiply two given arrays of same size element-by-element..
+    """
     mult = np.multiply(num1, num2)
     print(mult)
