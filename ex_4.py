26195f4c2a8bf27a18ed6344013eab1ba83e57a4
Esther Wahnon Amselem <82866031+estherwa@users.noreply.github.com>
2022-06-14 20:26:18 +0300

    Update ex_4.py

--- ex_4.py
+++ ex_4.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def ex_numpy_forth(x):
     """
-    Function to create a 10x10 matrix, in which the elements on the borders will be equal to 1, and inside 0.
+    A function that creates a 10x10 matrix, in which the elements on the borders will be equal to 1, and inside 0.
     """
     x = np.ones((10, 10))
     x[1:-1, 1:-1] = 0
