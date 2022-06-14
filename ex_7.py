5bf7929702255f33746f5448db0ad96fac40a1cb
Esther Wahnon Amselem <82866031+estherwa@users.noreply.github.com>
2022-06-14 20:45:03 +0300

    Update ex_7.py

--- ex_7.py
+++ ex_7.py
@@ -3,7 +3,9 @@ import numpy as np
 
 def ex_numpy_seven():
     """
-    Function to compute the x and y coordinates for points on a sine curve.
+    A function that creates an array with some values,
+    and the function must create a new array and swap the first and the last rows.
+  
     """
     nums = np.arange(1000, size=16).reshape(-1, 4)
 
