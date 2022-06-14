dab79c40f0e6f4fd8139b6c4c3483c30f7b66913
Esther Wahnon Amselem <82866031+estherwa@users.noreply.github.com>
2022-06-14 20:54:07 +0300

    Update ex_16.py

--- ex_16.py
+++ ex_16.py
@@ -3,7 +3,7 @@ import numpy as np
 
 def ex_numpy_sixteen():
     """
-     A NumPy program to create a three-dimension array with a specific shape .
+     A NumPy program that sorts the students ID with their heights  and id.
     """
     student_id = np.array([1, 2, 3, 4, 5, 6, 7])
     student_height = np.array([40., 42., 45., 41., 38., 40., 42.0])
