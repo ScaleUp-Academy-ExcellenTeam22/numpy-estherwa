eea2cc530d47b66cf3cc6b82ddab64b582610166
Esther Wahnon Amselem <82866031+estherwa@users.noreply.github.com>
2022-06-14 20:25:33 +0300

    Update ex_2.py

--- ex_2.py
+++ ex_2.py
@@ -5,6 +5,6 @@ def ex_numpy_second():
     """
     Function  that creates a vector of length 10 with values evenly distributed between 5 and 50.
     """
-    v = np.linspace(10, 49, 5)
+    v = np.linspace(5, 50, 10)
 
     print(v)
